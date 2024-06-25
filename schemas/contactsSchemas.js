d51f66648036fa8c14abac3b2c0cfc571db07867
Nataliia Hodnia <156535926+nataliiahodnia@users.noreply.github.com>
2024-06-25 13:39:40 +0200

    step 2

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,9 +1,55 @@
-import Joi from "joi";
+const fs = require("fs/promises");
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 
-export const createContactSchema = Joi.object({
+const contactsPath = path.join(__dirname, "db", "contacts.json");
 
-})
+async function readContacts() {
+  const data = await fs.readFile(contactsPath, "utf8");
+  return JSON.parse(data);
+}
 
-export const updateContactSchema = Joi.object({
+async function writeContacts(contacts) {
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2), "utf8");
+}
 
-})
+async function listContacts() {
+  return await readContacts();
+}
+
+async function getContactById(contactId) {
+  const contacts = await readContacts();
+  const contact = contacts.find((contact) => contact.id === contactId);
+  return contact || null;
+}
+
+async function removeContact(contactId) {
+  const contacts = await readContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [removedContact] = contacts.splice(index, 1);
+  await writeContacts(contacts);
+  return removedContact;
+}
+
+async function addContact(name, email, phone) {
+  const contacts = await readContacts();
+  const newContact = {
+    id: uuidv4(),
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  await writeContacts(contacts);
+  return newContact;
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};

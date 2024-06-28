5d8fdcc7b66b1747751ac03a372e7645e8f68584
Nataliia Hodnia <156535926+nataliiahodnia@users.noreply.github.com>
2024-06-28 12:56:50 +0200

    fixed

--- controllers/auth/authorization.js
+++ controllers/auth/authorization.js
@@ -1,69 +1,41 @@
-const { Schema, model } = require("mongoose");
-const Joi = require("joi");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-const { mongooseError } = require("../../middlewares/mongooseError");
+const { User } = require("../../models/user");
+const { HttpError } = require("../../helpers/HttpError");
+const { SECRET_KEY } = process.env;
 
-const phoneRegExp = /^\(\d{3}\) \d{3}-\d{4}$/;
+const authorization = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
 
-const contactSchema = new Schema(
-  {
-    name: {
-      type: String,
-      required: [true, "Set name for contact"],
-    },
-    email: {
-      type: String,
-      required: true,
-    },
-    phone: {
-      type: String,
-      match: [phoneRegExp, "Invalid phone number format. Use (XXX) XXX-XXXX"],
-      required: true,
-    },
-    favorite: {
-      type: Boolean,
-      default: false,
-    },
-    owner: {
-      type: Schema.Types.ObjectId,
-      ref: "user",
-    },
-  },
-  { versionKey: false, timestamps: true }
-);
+    if (!user) {
+      throw HttpError(401, "Email or password is wrong");
+    }
 
-contactSchema.post("save", mongooseError);
+    const passwordCompare = await bcrypt.compare(password, user.password);
 
-const createContactSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
-  favorite: Joi.boolean(),
-});
+    if (!passwordCompare) {
+      throw HttpError(401, "Email or password is wrong");
+    }
 
-const updateContactSchema = Joi.object({
-  name: Joi.string(),
-  email: Joi.string(),
-  phone: Joi.string(),
-  favorite: Joi.boolean(),
-});
+    const payload = {
+      id: user._id,
+    };
 
-const updateFavorite = Joi.object({
-  name: Joi.string(),
-  email: Joi.string(),
-  phone: Joi.string(),
-  favorite: Joi.boolean().required(),
-});
+    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "24h" });
+    await User.findByIdAndUpdate(user._id, { token });
 
-const schemas = {
-  createContactSchema,
-  updateContactSchema,
-  updateFavorite,
+    res.json({
+      email: user.email,
+      subscription: user.subscription,
+
+      token,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
-const Contact = model("contact", contactSchema);
-
-module.exports = {
-  Contact,
-  schemas,
-};
+module.exports = authorization;
